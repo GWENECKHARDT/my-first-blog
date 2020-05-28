63dbdf1cf997b77bcaa0d5836f649f241cf517da
GWENECKHARDT <s0573811@htw-berlin.de>
2020-05-28 21:44:25 +0200

    My Django Girls app, first commit

---
+++ djangovenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\gwen\documents\uni\sose 20\betriebliche_anwendungen\django\djangovenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
