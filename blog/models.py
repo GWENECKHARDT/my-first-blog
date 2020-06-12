f69892e7e7bf96c7bb487f900fc6df4677fe96e2
GWENECKHARDT <s0573811@htw-berlin.de>
2020-06-12 09:54:53 +0200

    View und Template für Blogpost-Details sowie CSS für die Website hinzugefügt

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
+    topic = models.CharField(max_length=150)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
